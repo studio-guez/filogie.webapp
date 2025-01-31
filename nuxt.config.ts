a125bc4f407e9adde33cf47f9b5e4f68a213fc55
azertypow <nico@villa1203.ch>
2025-01-31 09:14:13 +0100

    feat: init filogie content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    app: {
+        baseURL: '/',
+    },
+    ssr: false,
+    css: [
+        '@/assets/_main.scss'
+    ],
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,57 @@
 <template>
-    <section
-        class="v-index"
+    <section class="v-index"
     >
-      Filogie
+      <video class="v-index__video"
+             src="/videos/3001-FILOGIE_animation_bandeau_BD.mp4"
+             muted
+             loop
+             autoplay
+             playsinline
+      />
+
+      <div class="v-index__content app-grid-coll-container">
+        <div class="app-grid-coll-2-24 app-grid-coll-reg-2-24 v-index__content__order-3"></div>
+        <div class="app-grid-coll-22-24 app-grid-coll-reg-6-24 v-index__content__order-4">
+          <div class="app-first-and-last-child-remove-margin" style="margin-top: 1rem">
+            <h6>Direction</h6>
+
+            <p class="app-text-small"
+               style="padding-left: 1rem;"
+            >
+              <b>Nadia Cao</b>
+              <br/>Directrice
+            </p>
+
+            <h6>Conseil de Fondation</h6>
+
+            <p class="app-text-small"
+               style="padding-left: 1rem;"
+            >
+              <b>Bruno Marchand</b>
+              <br/>Président
+
+              <b><br>Luca Pattaroni</b>
+              <br/>Vice-président
+
+              <b><br>Nicolas Dzierlatka</b>
+              <br/>Secrétaire
+
+              <b><br>Marc Maugué</b>
+              <br/>Membre
+
+              <b><br>Jacques Roulet</b>
+              <br/>Conseiller
+            </p>
+          </div>
+        </div>
+        <div class="app-grid-coll-2-24 app-grid-coll-reg-0-24"></div>
+        <div class="app-grid-coll-22-24 app-grid-coll-reg-14-24">
+          <div style="margin-top: 1rem" class="app-first-and-last-child-remove-margin">
+            <p>La Fondation Immobilière pour repenser le Logement par l’Innovation et l’Expérimentation (FILOGIE) est née d'une volonté de proposer des solutions de logement adaptées aux enjeux contemporains et à ceux de demain. Face aux défis croissants liés au logement abordable, durable et inclusif, FILOGIE se fixe pour mission d’innover dans le domaine de l’habitat, en développant des projets et réalisations répondant aux besoins sociaux, économiques et environnementaux de notre société.</p>
+            <p>Partant du constat que le modèle familial dominant du second après-guerre – celui de la famille nucléaire – bien qu’encore présent, n’est plus majoritaire, FILOGIE s’attache à répondre aux attentes de nouveaux profils d’utilisateurs et à anticiper les évolutions et les besoins futurs en matière de logement abordable. Pour atteindre une innovation constante elle s’appuie aussi sur des études prospectives, des publications, ainsi que sur l’organisation d’événements et de manifestations, afin de produire et de partager des connaissances approfondies sur les enjeux actuels et à venir.</p>
+          </div>
+        </div>
+      </div>
     </section>
 </template>
 
@@ -23,7 +72,26 @@ const props = defineProps<{
 
 
 <style lang="scss" scoped >
-.v-index {
-  font-family: sans-serif;
+@use '@/assets/_scss-params';
+
+.v-index__video {
+  display: block;
+  width: 100%;
+}
+
+.v-index__content__order-3 {
+  order: 3;
+
+  @media (min-width: scss-params.$app-breakpoint-sm) {
+    order: initial;
+  }
+}
+
+.v-index__content__order-4 {
+  order: 4;
+
+  @media (min-width: scss-params.$app-breakpoint-sm) {
+    order: initial;
+  }
 }
 </style>

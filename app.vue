da87929afc6e764f8562d6c3d69b2051d4272d02
azertypow <nico@villa1203.ch>
2025-12-23 19:01:19 +0100

    feat: footer

--- app.vue
+++ app.vue
@@ -4,22 +4,22 @@
   </div>
   <footer>
     <div class="app-grid-coll-container"
-         style="padding-top: 2rem; padding-bottom: 1rem;"
+         style="padding-top: 1rem; padding-bottom: .5rem;"
     >
       <div class="app-grid-coll-0-24 app-grid-coll-reg-8-24"/>
       <div class="app-grid-coll-24-24 app-grid-coll-reg-16-24">
         <div class="app-grid-coll-container">
           <div class="app-grid-coll-24-24 app-grid-coll-reg-8-24 app-grid-with-gutter">
-            <h6>Adresse postale</h6>
-            <p>
-              Route de la Galaise 17
+            <p class="app-text-small">
+              <b>Adresse postale</b>
+              <br>Route de la Galaise 17
               <br/>1228 Plan-les-Ouates
             </p>
           </div>
           <div class="app-grid-coll-24-24 app-grid-coll-reg-8-24 app-grid-with-gutter">
-            <h6>Nous rencontrer</h6>
-            <p>
-              Route de la Galaise 15b
+            <p class="app-text-small">
+              <b>Nous rencontrer</b>
+              <br>Route de la Galaise 15b
               <br/>1228 Plan-les-Ouates
             </p>
           </div>

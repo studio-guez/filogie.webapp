d242840c671eea668101135bb032d447d1b2c7ff
OctoY <yann@octoplus.solutions>
2026-08-03 17:23:14 +0200

    fix: update team member name in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -116,7 +116,7 @@
               <h5>Ariane Widmer</h5>
               <p>Membre</p>
 
-              <h5>Valérie Défago-Gaudin</h5>
+              <h5>Valérie Défago</h5>
               <p>Membre</p>
 
               <h5>Jacques Roulet</h5>
